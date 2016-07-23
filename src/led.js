78d4eb88463ec087a0c94b3edd06dbc8f5bf7c81
Trybynenko Andrii <trybynenko@gmail.com>
2016-07-23 19:04:51 +0200

    Basic Led configuration added

---
+++ src/led.js
@@ -0,0 +1,17 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+    connections: {
+        arduino: {adaptor: 'firmata', port: 'COM3'}
+    },
+
+    devices: {
+        led: {driver: 'led', pin: 8}
+    },
+
+    work: function (my) {
+        every((1).second(), function () {
+            my.led.toggle();
+        });
+    }
+}).start();
