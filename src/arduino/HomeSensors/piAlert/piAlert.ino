e4f26202ede80db0ed6726f6c32fad84fd0ad378
Andrii Trybynenko <tamerjlah@gmail.com>
2016-08-15 17:12:54 +0200

    all pins

--- src/arduino/HomeSensors/piAlert/piAlert.ino
+++ src/arduino/HomeSensors/piAlert/piAlert.ino
@@ -1,3 +1,21 @@
+#include <IRremote.h>
+
+/* IR remote Sensor */
+int RECV_PIN = 11;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+
+/* RGB LED */
+const int RED_PIN = 4;
+//const int GREEN_PIN = 13;
+const int BLUE_PIN = 10;
+
+/* Define hardware connections Sound Sensor */
+#define PIN_GATE_IN 3
+#define IRQ_GATE_IN  0
+#define PIN_ANALOG_IN A0
+
 /* Pi PC */
 #define piPin 6                 // PI PC
 
@@ -5,9 +23,9 @@
 #define buzzerPin 12            // BUZZER
 
 /* PIR sensor */
-#define inputPin  2             // choose the input pin (for PIR sensor)
+#define inputPin  9             // choose the input pin (for PIR sensor)
 int pirState = LOW;             // we start, assuming no motion detected
-int val = 0;                    // variable for reading the pin status
+int PIRval = 0;                    // variable for reading the pin status
 
 /* US sensor */
 #define echoPin 7               // Echo Pin
@@ -16,29 +34,10 @@ int maximumRange = 200;         // Maximum range needed
 int minimumRange = 0;           // Minimum range needed
 long duration, distance;        // Duration used to calculate distance
 
-void setup() {
-  //Serial.begin (9600); ///////////////
-
-  /* Pi PC */
-  pinMode(piPin, OUTPUT); 
-
-  /* Sound Buzzer */
-  pinMode(buzzerPin, OUTPUT);
-
-  /* US sensor */
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
-  /* PIR sensor */
-  pinMode(inputPin, INPUT);  
-  
-  //delay(30000); //30seconds delay before start
-}
-
+/* PIR SENSOR */
 void motionDetect(){
-    // PIR SENSOR
-  val = digitalRead(inputPin); 
-  if (val == HIGH) {            
+  PIRval = digitalRead(inputPin); 
+  if (PIRval == HIGH) {            
     if (pirState == LOW) {
       pirState = HIGH;
       digitalWrite(piPin, HIGH);
@@ -51,9 +50,9 @@ void motionDetect(){
   }
 }
 
+/*  UltraSonic Sensor */
 void ultraSonic(){ 
-  /*  UltraSonic Sensor */
-  
+
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
 
@@ -71,9 +70,61 @@ void ultraSonic(){
   }
 }
 
+
+void setup() {
+  Serial.begin(9600);
+//  Serial.flush();
+
+  pinMode(RED_PIN, OUTPUT);
+//  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  
+  /* configure input to interrupt sound sensor */
+  pinMode(PIN_GATE_IN, INPUT);
+  attachInterrupt(IRQ_GATE_IN, soundISR, CHANGE);
+
+  /* Pi PC */
+  pinMode(piPin, OUTPUT); 
+
+  /* Sound Buzzer */
+  pinMode(buzzerPin, OUTPUT);
+
+  /* US sensor */
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  /* PIR sensor */
+  pinMode(inputPin, INPUT);  
+
+  /* IR Remote */
+  irrecv.enableIRIn(); // Start the receiver
+
+  delay(7000);
+  digitalWrite(BLUE_PIN, "HIGH"); 
+}
+
+
+void soundISR()
+{
+  if(digitalRead(PIN_GATE_IN) == 1){
+    //digitalWrite(GREEN_PIN, "HIGH"); 
+    //digitalWrite(piPin, "HIGH"); 
+      Serial.print("gate : 1");
+  } else {
+    //digitalWrite(piPin, "LOW"); 
+    Serial.print("gate : 2");
+  }
+}
+
+
 void loop() {
   ultraSonic();
   motionDetect();
 
-  delay(50);
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+
+  delay(100);
 }
---
+++ src/arduino/encoding/encoding.ino
@@ -0,0 +1,30 @@
+/*
+  Analog input
+
+    Reads an analog input on analog in 0, and prints the value out.
+
+    Created 24 March 2006
+    by Tom Igoe
+*/
+
+int analogValue = 0;    // Variable to hold the analog value.
+
+void setup() {
+    // open the serial port at 9600 bit/s:
+    Serial.begin(9600);
+}
+
+void loop() {
+    // Read the analog input on pin 0:
+    analogValue = analogRead(0);
+
+    // Print it out in many formats:
+    Serial.println(analogValue);       // print as an ASCII-encoded decimal
+    Serial.println(analogValue, DEC);  // print as an ASCII-encoded decimal
+    Serial.println(analogValue, HEX);  // print as an ASCII-encoded hexadecimal
+    Serial.println(analogValue, OCT);  // print as an ASCII-encoded octal
+    Serial.println(analogValue, BIN);  // print as an ASCII-encoded binary    
+
+    // Delay 10 milliseconds before the next reading:
+    delay(10);
+}
